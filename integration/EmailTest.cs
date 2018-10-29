398688cfedbba11c10ba8296edf10a5f05b48f4d
DAHMAN Rami <rami.dahman@soprasteria.com>
2018-10-29 08:43:25 +0100

    Integration Test project

---
+++ integration/EmailTest.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace integration
+{
+    public class EmailTest
+    {
+        public const string GeneratorApiRoot = "http://generator";
+        [Fact]
+        public async Task Test1()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"{GeneratorApiRoot}/EmailRandomNames")
+            };
+
+            using (var response= await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+            }
+        }
+    }
+}
